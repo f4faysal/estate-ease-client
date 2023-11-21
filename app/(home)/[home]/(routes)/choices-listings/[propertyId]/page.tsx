673e7699f297f1829652ccc2c8adfe69917ddc08
f4faysal <f4faysals@gmail.com>
2023-11-21 20:18:12 +0600

    Refactor property details page layout

--- app/(home)/[home]/(routes)/choices-listings/[propertyId]/page.tsx
+++ app/(home)/[home]/(routes)/choices-listings/[propertyId]/page.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import Container from "@/components/ui/container";
 
-const PropertyDetails = () => {
+interface Props {
+  params: {
+    propertyId: string;
+    home: string;
+  };
+}
+
+const PropertyDetails = ({ params }: Props) => {
   return (
     <div>
       <div className="w-full h-[300px] bg-[#EBF6FF] rounded-lg"></div>
 
       <Container>
-        <h1>hello</h1>
+        <div className="grid grid-cols-4 grid-rows-2 gap-4">
+          <div className="col-span-2 row-span-2">1</div>
+          <div className="">2</div>
+          <div className="">3</div>
+          <div className="">4</div>
+          <div className="">5</div>
+        </div>
+
+        <h1>{params.propertyId}</h1>
       </Container>
     </div>
   );
