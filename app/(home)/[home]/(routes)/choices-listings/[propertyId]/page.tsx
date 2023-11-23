5a4fb8d095851d6a39101a27a58c5c2ef693b701
f4faysal <f4faysals@gmail.com>
2023-11-23 14:25:30 +0600

    Refactor property details page layout

--- app/(home)/[home]/(routes)/choices-listings/[propertyId]/page.tsx
+++ app/(home)/[home]/(routes)/choices-listings/[propertyId]/page.tsx
@@ -23,6 +23,17 @@ const PropertyDetails = ({ params }: Props) => {
       <div className="w-full h-[300px] bg-[#EBF6FF] rounded-lg"></div>
 
       <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8 -mt-[300px]">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <Gallery images={data?.home.images} />
+
+            {/* <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div> */}
+          </div>
+          <hr className="my-10" />
+          {/* <ProductList title="Related Items" items={suggestedProducts} /> */}
+        </div>
         <div className="grid grid-cols-4 grid-rows-2 gap-4">
           <div className="col-span-2 row-span-2 h-80 border">1</div>
           <div className="border">2</div>
@@ -30,8 +41,6 @@ const PropertyDetails = ({ params }: Props) => {
           <div className="border">4</div>
           <div className="border">5</div>
         </div>
-
-        <Gallery images={data?.home.images} />
 
         <h1>{data?.home.title}</h1>
       </Container>
