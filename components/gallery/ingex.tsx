f9c35b54fe1c831841af0d7330ce7c23109cdbb4
f4faysal <f4faysals@gmail.com>
2023-11-24 19:13:13 +0600

    Update gallery layout in ingex.tsx

--- components/gallery/ingex.tsx
+++ components/gallery/ingex.tsx
@@ -13,12 +13,12 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({ images = [] }) => {
   return (
-    <Tab.Group as="div" className="grid grid-cols-2  gap-4 ">
-      <div>
+    <Tab.Group as="div" className="grid grid-cols-4 grid-rows-2  gap-4 ">
+      <div className="col-span-2 row-span-2">
         <Tab.Panels className="aspect-square w-full">
           {images.map((image) => (
             <Tab.Panel key={image.id}>
-              <div className="aspect-square relative h-96 w-full sm:rounded-lg overflow-hidden">
+              <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
                 <NextImage
                   fill
                   src={image.url}
@@ -31,8 +31,8 @@ const Gallery: React.FC<GalleryProps> = ({ images = [] }) => {
         </Tab.Panels>
       </div>
 
-      <div className="mx-auto hidden w-full max-w-2xl sm:block lg:max-w-none">
-        <Tab.List className="grid grid-cols-3 grid-rows-2 gap-4">
+      <div className="col-span-2 row-span-2 mx-auto hidden w-full max-w-2xl sm:block lg:max-w-none">
+        <Tab.List className="grid grid-cols-2 grid-rows-2 gap-4">
           {images.map((image) => (
             <GalleryTab key={image.id} image={image} />
           ))}
